f3ed6d9d810e385972cf7df1c00b0433687232e1
Treasure <mac@MACs-MacBook-Pro.local>
2023-05-11 10:56:14 +0100

    first commit

---
+++ Python-Questions-Answers.py
@@ -0,0 +1,68 @@
+#question 1
+
+# programme_COCS = ['Computer Science' , 'Software Enginering' , 'Cyber Security' , 'Mass Computer ' , 'Information Technology' , 'Communication Arts']
+# for programme_COCS in programme_COCS:
+#     print(programme_COCS)
+
+#question 2
+
+# import math
+# radius = float(input("Enter the radius of the circle: "))
+# area = math.pi * radius ** 2
+# print("The area of the circle is : ", area)
+
+#QUESTION 3
+
+# first_name = input(str("What is your first name: "))
+# last_name = input(str("What is your last name: "))
+# print(last_name , first_name)
+
+#QUESTION 4
+
+# input_string = input("Enter a numbers: ")
+# number_list = input_string.split(",")
+# number_tuple = tuple(number_list)
+# print("list: ",number_list)
+# print("tuple: ",number_tuple)
+
+#QUESTION 5
+
+# colors_list = ['Red', 'White' , 'Purple' , 'Black']
+# print(colors_list[0])
+# print(colors_list[-1])
+
+#QUESTION 6
+
+# n = int(input("Enter a number: "))
+# final_result = n + n * n + n ** 4
+# print(final_result)
+
+#QUESTION 7
+
+# import calendar
+# month = int(input("Enter a month: "))
+# year = int(input("Enter a year: "))
+# print(calendar.month(year, month))
+
+#question 8
+
+# from datetime import date
+# date1 = date(2021,5,1)
+# date2= date(2021,5,15)
+# date3= date2 - date1
+# print(date3.days)
+
+#QUESTION 9
+
+# import math
+# radius = 6
+# volume = (4/3) * math.pi * radius ** 3
+# print(volume)
+
+#question 10
+
+# number = int(input("Enter a number: "))
+# if (number %2 == 0):
+#     print("The number is even")
+# else:
+#     print("The number is odd")    
